5731c2c4f1c580aefd6c3c8c763e2b64896c1a39
Nael Hubaishi <nael.hub@gmail.com>
2023-01-21 23:35:21 -0500

    styling update

--- src/App.vue
+++ src/App.vue
@@ -162,4 +162,85 @@ form input {
   text-transform: uppercase;
   transition: 0.4s;
 }
+
+.button:hover {
+  background-position: right;
+}
+.results {
+  background-color: #fff;
+  border-radius: 0.5rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  max-height: 480px;
+  overflow-y: scroll;
+  margin-bottom: 1.5rem;
+}
+.result {
+  display: flex;
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  transition: 0.4s;
+}
+.result img {
+  width: 100px;
+  border-radius: 1rem;
+  margin-right: 1rem;
+}
+.details {
+  flex: 1 1 0%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+}
+.details h3 {
+  font-size: 1.25rem;
+  margin-bottom: 0.5rem;
+}
+.details p {
+  font-size: 0.875rem;
+  margin-bottom: 1rem;
+}
+.details .button {
+  margin-left: auto;
+}
+.flex-1 {
+  display: block;
+  flex: 1 1 0%;
+}
+.myanime h2 {
+  color: #888;
+  font-weight: 400;
+  margin-bottom: 1.5rem;
+}
+.myanime .anime {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.5rem;
+  background-color: #fff;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
+.anime img {
+  width: 72px;
+  height: 72px;
+  object-fit: cover;
+  border-radius: 1rem;
+  margin-right: 1rem;
+}
+.anime h3 {
+  color: #888;
+  font-size: 1.125rem;
+}
+.anime .episodes {
+  margin-right: 1rem;
+  color: #888;
+}
+.anime .button:first-of-type {
+  margin-right: 0.5rem;
+}
+.anime .button:last-of-type {
+  margin-right: 0;
+}
 </style>
